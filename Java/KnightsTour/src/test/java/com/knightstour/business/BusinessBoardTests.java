7062ae7a308d1bcd741a47d9dd8ecbf810a439fc
Juliano Ladeira <julianoladeira@gmail.com>
2021-03-29 02:59:08 -0300

    Test adjustments

--- Java/KnightsTour/src/test/java/com/knightstour/business/BusinessBoardTests.java
+++ Java/KnightsTour/src/test/java/com/knightstour/business/BusinessBoardTests.java
@@ -9,12 +9,12 @@ public class BusinessBoardTests {
 
     @Test(expected = IllegalArgumentException.class)
     public void ShouldThrowErrorOnIllegalBoardSize() {
-        BusinessBoard board = new BusinessBoard(0,0, new Position(0,0));
+        new BusinessBoard(0,0, new Position(0,0));
     }
 
     @Test
     public void ShouldHaveNoSolutionForImpossibleBoard() {
-        BusinessBoard board = new BusinessBoard(1,2, new Position(0,0));
+        var board = new BusinessBoard(1,2, new Position(0,0));
         var result = board.findSolution();
         assertNull(result);
     }
